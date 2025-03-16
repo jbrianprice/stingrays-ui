38b5b0a957cbfcf07fb1162364505dee42dc2ecd
jbrianprice <jbrianprice@gmail.com>
2025-03-15 22:20:49 -0400

    af

--- src/components/teamSelector.jsx
+++ src/components/teamSelector.jsx
@@ -34,41 +34,42 @@ const TeamSelector = () => {
                 )}
             </div>
             {(selector || selectedTeams.length === 0) && (
-                <form className="contents" onClick={() => showSelector(!selector)}>
-                    <div className="flex flex-col gap-4 text-center border-b pb-6 mb-6 border-neutral-200 dark:border-neutral-800 ">
-                        <h3 className="font-semibold text-stone-500 py-2">Who's playing?</h3>
+                <div className="flex flex-col gap-4 text-center border-b pb-6 mb-6 border-neutral-200 dark:border-neutral-800 ">
+                    <h3 className="font-semibold text-stone-500 py-2">Who's playing?</h3>
 
-                        <div className="card flex flex-col gap-4">
-                            <ul className="flex flex-col gap-2">
-                                {Object.entries(groupedByTeam).map(([teamName, players]) => (
-                                    <li
-                                        key={teamName}
-                                        className="text-left tertiary cardx flex gap-3 border-b last:border-none border-neutral-200 dark:border-neutral-800 pb-2 last:pb-0"
-                                        onClick={() => handleSelectionChange(teamName)}
-                                    >
-                                        {selectedTeams.includes(teamName) ? (
-                                            <CheckCircle2 className=" text-cyan-600x text-white bg-green-500 rounded-full" />
-                                        ) : (
-                                            <Circle className="text-stone-500" />
-                                        )}
+                    <div className="card flex flex-col gap-4">
+                        <ul className="flex flex-col gap-2">
+                            {Object.entries(groupedByTeam).map(([teamName, players]) => (
+                                <li
+                                    key={teamName}
+                                    className="text-left tertiary cardx flex gap-3 border-b last:border-none border-neutral-200 dark:border-neutral-800 pb-2 last:pb-0"
+                                    onClick={() => handleSelectionChange(teamName)}
+                                >
+                                    {selectedTeams.includes(teamName) ? (
+                                        <CheckCircle2 className=" text-cyan-600x text-white bg-green-500 rounded-full" />
+                                    ) : (
+                                        <Circle className="text-stone-500" />
+                                    )}
 
-                                        <div>
-                                            <h4 className="text-normal text-neutral-950x dark:text-whitex font-semibold">
-                                                {teamName}
-                                            </h4>
-                                            <p>{players.length} players</p>
-                                        </div>
-                                    </li>
-                                ))}
-                            </ul>
-                            {selector && selectedTeams.length > 0 && (
-                                <button type="submit" className="secondary w-full">
-                                    Apply
-                                </button>
-                            )}
-                        </div>
+                                    <div>
+                                        <h4 className="text-normal text-neutral-950x dark:text-whitex font-semibold">
+                                            {teamName}
+                                        </h4>
+                                        <p>{players.length} players</p>
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
+                        {selector && selectedTeams.length > 0 && (
+                            <button
+                                className="secondary w-full"
+                                onClick={() => showSelector(!selector)}
+                            >
+                                Apply
+                            </button>
+                        )}
                     </div>
-                </form>
+                </div>
             )}
         </div>
     )
